ffb635e62017ee62c2c264952e309d47e3771b0a
hagyeom <116890147+hagyeom@users.noreply.github.com>
2024-05-04 04:06:44 +0900

    commit

---
+++ bronze_5/boj_15963.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n, m;
+    cin >> n >> m;
+    if (n == m)
+    {
+        cout << 1;
+    }
+    else if (n != m)
+    {
+        cout << 0;
+    }
+
+    return 0;
+}
